fc7816ab07b1b7dbdaf205a0bdbfd3535d8445a8
Bae Janghun <pain3992@naver.com>
2020-12-16 17:17:28 +0900

    Test Commit

--- src/axios.js
+++ src/axios.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
 	// baseURL: 'https://us-central1-challenge-e9967.cloudfunctions.net/api'
 	baseURL: 'http://localhost:5001/challenge-e9967/us-central1/api'
+	// Test Commit
 });
 
 export default instance;
